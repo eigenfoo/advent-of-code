1ce7578b0835e0d1e5eba8af3684afe9f77d3b76
George Ho <19851673+eigenfoo@users.noreply.github.com>
2020-12-01 20:34:12 -0500

    Starting 2020 again lol

---
+++ 2020/day-01/solution.jl
@@ -0,0 +1,26 @@
+lines = readlines("input.txt")
+
+expenses = map(x -> parse(Int, x), lines)
+numexpenses = length(expenses)
+
+for i in 1:numexpenses
+    for j in 1:i
+        if expenses[i] + expenses[j] == 2020
+            println(expenses[i] * expenses[j])
+        end
+    end
+end
+
+# ---
+
+for i in 1:numexpenses
+    for j in 1:i
+        for k in 1:j
+            if expenses[i] + expenses[j] + expenses[k] == 2020
+                println(expenses[i] * expenses[j] * expenses[k])
+            end
+        end
+    end
+end
+
+
