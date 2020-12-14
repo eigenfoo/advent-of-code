53384f1fb1d168ccd7b33d5c1a0d03e747bcb8ae
George Ho <19851673+eigenfoo@users.noreply.github.com>
2020-12-14 11:27:27 -0500

    Finish day 14

---
+++ 2020/day-14/solution.jl
@@ -0,0 +1,61 @@
+function binary36bits(s)
+    bitstring(parse(Int, s))[29:end]  # Only 36 bits
+end
+
+
+lines = readlines("input.txt")
+
+memory = Dict()
+mask = repeat("X", 36)
+regex = r"mem\[(\d*)\] = (\d*)"
+
+for line in lines
+    global memory, mask, regex
+    if startswith(line, "mask")
+        mask = line[8:end]
+    elseif startswith(line, "mem")
+        address, value = match(regex, line).captures
+        address = parse(Int, address)
+        value = binary36bits(value)
+        maskedvalue =
+            parse(Int, join([m == 'X' ? v : m for (m, v) in zip(mask, value)]), base = 2)
+        memory[address] = maskedvalue
+    end
+end
+
+println(sum(values(memory)))
+
+# ---
+
+function addresses(maskedaddress)
+    addresses = [maskedaddress]
+    while occursin('X', addresses[1])
+        address = popfirst!(addresses)
+        push!(addresses, replace(address, 'X' => '0', count = 1))
+        push!(addresses, replace(address, 'X' => '1', count = 1))
+    end
+    map(x -> parse(Int, x, base = 2), addresses)
+end
+
+
+memory = Dict()
+mask = repeat("X", 36)
+regex = r"mem\[(\d*)\] = (\d*)"
+
+for line in lines
+    global memory, mask, regex
+    if startswith(line, "mask")
+        mask = line[8:end]
+    elseif startswith(line, "mem")
+        address, value = match(regex, line).captures
+        address = binary36bits(address)
+        value = parse(Int, value)
+        maskedaddress = join([m == '0' ? a : m for (m, a) in zip(mask, address)])
+        addresses_ = addresses(maskedaddress)
+        for address in addresses_
+            memory[address] = value
+        end
+    end
+end
+
+println(sum(values(memory)))
